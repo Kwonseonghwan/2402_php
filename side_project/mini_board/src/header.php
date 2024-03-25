6de6b5db42fa908cf500ba0520c7d0b44b6a2519
Kwonseonghwan <ksh5145@gmail.com>
2024-03-25 18:07:33 +0900

    2403 php

---
+++ side_project/mini_board/src/header.php
@@ -0,0 +1,46 @@
+<?php
+
+?>
+
+<div class="snowflakes" aria-hidden="true">
+                <div class="snowflake">
+                  <div class="inner">❅</div>
+                </div>
+                <div class="snowflake">
+                  <div class="inner">❅</div>
+                </div>
+                <div class="snowflake">
+                  <div class="inner">❅</div>
+                </div>
+                <div class="snowflake">
+                  <div class="inner">❅</div>
+                </div>
+                <div class="snowflake">
+                  <div class="inner">❅</div>
+                </div>
+                <div class="snowflake">
+                  <div class="inner">❅</div>
+                </div>
+                <div class="snowflake">
+                  <div class="inner">❅</div>
+                </div>
+                <div class="snowflake">
+                  <div class="inner">❅</div>
+                </div>
+                <div class="snowflake">
+                  <div class="inner">❅</div>
+                </div>
+                <div class="snowflake">
+                  <div class="inner">❅</div>
+                </div>
+                <div class="snowflake">
+                  <div class="inner">❅</div>
+                </div>
+                <div class="snowflake">
+                  <div class="inner">❅</div>
+                </div>
+              </div>
+<header>
+<h1><a href="./list.php">mini Board</a></h1>
+</header>
+            
