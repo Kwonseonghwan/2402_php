cdbb282d451feb54d6e055e12b3691f3f69759ca
Kwonseonghwan <ksh5145@gmail.com>
2024-04-01 10:00:24 +0900

    2403 php

--- side_project/111111.php
+++ side_project/111111.php
@@ -7,4 +7,4 @@ $end_page = $start_page + ($btn_page_cnt - 1);
 $end_page = $end_page > $max_page_num ? $max_page_num : $end_page;
 for($i = $start_page; $i <= $end_page; $i++) {
     echo "$i page\n";
-}
+}  
