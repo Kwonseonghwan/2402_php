e1bf9809054f8fa3fbe9daa7077a83f5e3243547
kwonseonghwan <ksh5145@gmail.com>
2024-05-20 17:22:32 +0900

    0520 vuestagram

---
+++ side_project/vuestagram/resources/js/store.js
@@ -0,0 +1,26 @@
+import {createStore} from 'vuex';
+
+const store = createStore({
+    state() {
+        return {
+
+        }
+    },
+    mutations: {
+
+    },
+    actions: {
+        // 인증관련
+        /**
+         * 
+         * @param {*} context 
+         * @param {*} userInfo 
+         */
+        login(context, userInfo) {
+            console.log(JSON.stringify(userInfo));
+            const url = '/api/login';
+        }
+    }
+});
+
+export default store;
