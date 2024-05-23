d8f846b95c44b5b00b8006e0cfb7603cac563825
kwonseonghwan <ksh5145@gmail.com>
2024-05-23 17:45:43 +0900

    0523 vuestagram 작성

--- side_project/vuestagram/app/Http/Controllers/BoardController.php
+++ side_project/vuestagram/app/Http/Controllers/BoardController.php
@@ -6,6 +6,7 @@ use App\Exceptions\MyValidateException;
 use App\Models\Board;
 use MyBoardValidate;
 use Illuminate\Http\Request;
+use MyToken;
 
 class BoardController extends Controller
 {
@@ -49,4 +50,78 @@ class BoardController extends Controller
         ];
         return response()->json($responseData, 200);
     }
+    /**
+     * 추가 보드리스트 획득
+     * 
+     * @return response() json
+     */
+    public function addIndex($id) {
+        // 유효성 체크용 데이터 초기화
+        $requestData = [
+            'id' => $id
+        ];
+        // 유효성 체크
+        $validator = MyBoardValidate::myValidate($requestData);
+
+        // 유효성 검사 실패시 처리
+        if($validator->fails()) {
+            throw new MyValidateException('E01');
+        }
+
+        // 게시글 정보 획득 
+        $BoardList = Board::join('users', 'boards.user_id', '=', 'users.id')
+                            ->select('boards.*','users.name')
+                            ->orderBy('boards.id', 'DESC')
+                            ->where('boards.id', '<', $id)
+                            ->limit(20)
+                            ->get();
+                
+        $responseData = [
+            'code' => '0',
+            'msg' => '',
+            'data' => $BoardList->toArray()
+        ];
+        return response()->json($responseData, 200);
+    }
+    /**
+     * 글 작성
+     * 
+     * @param Illuminate\Http\Request $request
+     * 
+     * @return string json
+     */
+    public function store(Request $request) {
+        // 유효성 체크용 데이터 초기화
+        $requestData = [
+            'content' => $request->content,
+            'img' => $request->img
+        ];
+        // 유효성 체크
+        $validator = MyBoardValidate::myValidate($requestData);
+
+        // 유효성 검사 실패시 처리
+        if($validator->fails()) {
+            throw new MyValidateException('E01');
+        }
+
+        // insert 데이터 작성
+        $insertData = $request->only('content');
+        $insertData['user_id'] = MyToken::getValueInPayload($request->bearerToken(), 'idt');
+        $filePath = $request->file('img')->store('img');
+        $insertData['img'] = '/'.$filePath;
+        $insertData['like'] =0;
+
+        //insert 처리
+        $BoardInfo = Board::create($insertData);
+
+        // response 데이터 생성
+        $responseData = [
+            'code' => '0'
+            ,'msg' => ''
+            ,'data' => $BoardInfo
+        ];
+
+        return response()->json($responseData, 200);
+    }
 }
+
--- side_project/vuestagram/resources/js/store.js
+++ side_project/vuestagram/resources/js/store.js
@@ -9,26 +9,42 @@ const store = createStore({
             userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : {},
             boardList: [],
             lastID: localStorage.getItem('lastID') ? localStorage.getItem('lastID') : 0,
+            noMoreBoardListFlg: false,
         }
     },
     mutations : {
         // ------------
         // 인증 관련
         // ------------
+        setNoMoreBoardListFlg(state, flg) {
+            state.noMoreBoardListFlg = flg;
+        },
         setAuthFlg(state, boo) {
             state.authFlg = boo;
         },
+        
+        // 유저 정보 관련
         setUserInfo(state, userInfo) {
             state.userInfo = userInfo;
         },
-
+        setUserBoardsCount(state) {
+            state.userInfo.boards_count++;
+        },
+        
         // 게시글 관련
          setBoardList(state, data) {
             state.boardList = data;
          },
          setLastID(state, id) {
             state.lastID = id;
-         }
+         },
+         setConcatBoardList(state, data) {
+            state.boardList = state.boardList.concat(data);
+         },
+         setUnshiftBoardList(state, data) {
+            state.boardList.unshift(data);
+         },
+
 
     },
     actions: {
@@ -76,6 +92,13 @@ const store = createStore({
             .then(response => {
                 console.log(response);
                 alert('logout');
+            })
+            .catch(error => {
+                console.log(error);
+                console.log(error.response);
+                alert('문제가 생겨 로그아웃 처리');
+            })
+            .finally(() => {
                 // 로컬 스토리지 비우기
                 localStorage.clear();
 
@@ -85,7 +108,6 @@ const store = createStore({
 
                 // 로그인으로 이동
                 router.replace('/login');
-            
             });
         },
         /**
@@ -118,6 +140,85 @@ const store = createStore({
                 console.log(error.response);
                 const code = error.response ? error.response.data.code : '';
                 alert('게시글 습득에 실패했습니다 ('+ code + ')');
+            });
+        },
+        /**
+         * 추가 게시글 리스트 획득
+         * 
+         * @param {*} context 
+         */
+        getAddBoardList(context) {
+            const url = '/api/board/' + context.state.lastID;
+            const config = {
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('accessToken'),
+                }
+            }
+            // axios 처리
+            axios.get(url, config)
+            .then(response => {
+                const data = response.data.data;
+                if(data.length > 0) {
+                    // 게시글 정보가 있을때 처리
+                    // 추가 게시글 저장
+                    context.commit('setConcatBoardList', data);
+                    
+                    // 마지막 게시글 id 저장
+                    context.commit('setLastID', data[data.length - 1].id);
+                    localStorage.setItem('lastID', data[data.length - 1].id);
+                } else {
+                    // 게시글 정보가 없을때 처리
+                    // 더이상 게시글이 없기 때문에 서버에 요청을 하지않기 위한 플래그 true로 세팅
+                    context.commit('setNoMoreBoardListFlg', true); 
+                    context.commit('setLastID',0);
+                    localStorage.removeItem('lastID');
+                }
+                console.log('추가 게시글 획득');
+            })
+            .catch(error => {
+                console.log(error);
+                console.log(error.response);
+                const code = error.response ? error.response.data.code : '';
+                alert('게시글 습득에 실패했습니다 ('+ code + ')');
+            });
+        },
+        /**
+         * 게시글 작성
+         * @param {store} context 
+         * @param {object} boardInfo 
+         */
+        storeBoard(context, boardInfo) {
+            const url = '/api/board';
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization' : 'Bearer ' + localStorage.getItem('accessToken'),
+                }
+            }
+            const data = new FormData();
+            data.append('content', boardInfo.content);
+            data.append('img', boardInfo.img);
+
+            // axios 처리
+            axios.post(url, data, config)
+            .then(response => {
+                if(context.state.boardList.length > 1) {
+                    // 보드리스트의 가장 앞에 작성한 글 정보 추가
+                    context.commit('setUnshiftBoardList', response.data.data);
+                }
+
+                // 유저의 작성글 수 1증가
+                context.commit('setUserBoardsCount');
+                localStorage.setItem('userInfo', JSON.stringify(context.state.userInfo));
+
+                // 게시글 인덱스로 이동
+                router.replace('/board');
+            })
+            .catch(error => {
+                // console.log(error);
+                // console.log(error.response);
+                const code = error.response ? error.response.data.code : '';
+                alert('게시글 작성에 실패했습니다 ('+ code + ')');
             });
         }
     }
